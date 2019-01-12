e028e679aba83560c43230f8000fa4c6ed193d4b
GOSSII <dipsgos7@gmail.com>
2019-01-12 11:02:58 -0500

    First Commit

---
+++ script/app.js
@@ -0,0 +1,11 @@
+(function(){
+
+    
+    function Start(){
+        let startValue = 0;
+        console.log(`%cApp Started..${startValue}`,"font-size :20px;color: green;")
+    }
+
+    window.addEventListener("load",Start);
+    console._commandLineAPI.clear();
+})();
