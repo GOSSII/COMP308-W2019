f462184063336498c03ebdd26d0d696563e65463
DIPESHPURI GOSWAMI <dipeshpuri9@gmail.com>
2019-01-26 12:28:44 -0500

    Assignment 3

--- scaffolding/routes/index.js
+++ scaffolding/routes/index.js
@@ -3,7 +3,22 @@ let router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home' });
 });
 
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: 'About' });
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('index', { title: 'Contact' });
+});
+
+router.get('/product', function(req, res, next) {
+  res.render('index', { title: 'Product' });
+});
+
+router.get('/services', function(req, res, next) {
+  res.render('index', { title: 'Services' });
+});
 module.exports = router;
