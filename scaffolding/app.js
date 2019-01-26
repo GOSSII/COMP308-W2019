cd52a69ef936c70d335918383476f36b443a431b
DIPESHPURI GOSWAMI <dipeshpuri9@gmail.com>
2019-01-26 12:00:26 -0500

    restructur Files

--- scaffolding/app.js
+++ scaffolding/app.js
@@ -1,13 +1,14 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+let createError = require('http-errors');
+let express = require('express');
+let path = require('path');
+let cookieParser = require('cookie-parser');
+let logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+let indexRouter = require('./routes/index');
+let usersRouter = require('./routes/users');
 
-var app = express();
+
+let app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -18,6 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'node_modules')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
---
+++ scaffolding/public/scripts/app.js
@@ -0,0 +1,9 @@
+/* Custome javascript goes here */
+// IIFE
+(   function(){
+    function Start(){
+        console.log("%c App Started..")
+    }
+
+    window.addEventListener("load",Start);
+})();
